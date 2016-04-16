505be7d5d7ff87dee12285ae06d527365642b97a
hennessytj <timothy.hennessy@trojans.dsu.edu>
2016-04-16 06:38:40 -0500

    skeleton for another good test

---
+++ float_array_test.c
@@ -0,0 +1,11 @@
+// declare float array of certain SIZE
+
+// loop until SIZE float grades have been put in array from user input
+
+// find average of grades
+
+// find highest grade
+
+// find lowest grade
+
+
